281219e16316b8ec7dc39846c314d4cf4874d240
malopro <malovagilbert@gmail.com>
2024-08-26 20:12:11 +0300

    Added game round functionality

--- Rock paper scissors/script.js
+++ Rock paper scissors/script.js
@@ -1,8 +1,26 @@
 const choice = ['rock', 'paper', 'scissors'];
-
+let humanScore = 0;
+let computerScore = 0;
 
 let getComputerChoice = () => {
     return choice[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice())
+let playRound = (humanChoice, computerChoice) => {
+    humanChoice = prompt().toLocaleLowerCase();
+    computerChoice = getComputerChoice();
+
+    if ((humanChoice === 'rock' && computerChoice === 'rock') || (humanChoice === 'paper' && computerChoice === 'paper') || (humanChoice === 'scissors' && computerChoice === 'scissors')) {
+        console.log('Draw');
+    }
+    if ((humanChoice === 'rock' && computerChoice === 'paper') || (humanChoice === 'paper' && computerChoice === 'scissors') || (humanChoice === 'scissors' && computerChoice === 'rock')) {
+        computerScore++;
+        console.log('You Lose!!');
+    }
+    if ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === 'scissors' && computerChoice === 'paper')) {
+        humanScore++;
+        console.log('You Win');
+    }
+}
+playRound()
+console.log(`Human score:${humanScore} \nComputer score:${computerScore}`)
