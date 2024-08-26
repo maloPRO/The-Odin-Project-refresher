92c43f7f318ea52030cbce0f6c0d5fd561432708
malopro <malovagilbert@gmail.com>
2024-08-26 21:36:12 +0300

    updated  game round functionality

--- Rock paper scissors/script.js
+++ Rock paper scissors/script.js
@@ -1,26 +1,37 @@
 const choice = ['rock', 'paper', 'scissors'];
-let humanScore = 0;
-let computerScore = 0;
 
 let getComputerChoice = () => {
     return choice[Math.floor(Math.random() * 3)];
 }
 
-let playRound = (humanChoice, computerChoice) => {
-    humanChoice = prompt().toLocaleLowerCase();
-    computerChoice = getComputerChoice();
+const getHumanChoice = () => {
+    return val = prompt().toLowerCase();
+} 
 
-    if ((humanChoice === 'rock' && computerChoice === 'rock') || (humanChoice === 'paper' && computerChoice === 'paper') || (humanChoice === 'scissors' && computerChoice === 'scissors')) {
-        console.log('Draw');
-    }
-    if ((humanChoice === 'rock' && computerChoice === 'paper') || (humanChoice === 'paper' && computerChoice === 'scissors') || (humanChoice === 'scissors' && computerChoice === 'rock')) {
-        computerScore++;
-        console.log('You Lose!!');
-    }
-    if ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === 'scissors' && computerChoice === 'paper')) {
-        humanScore++;
-        console.log('You Win');
+
+let playRound = (humanChoice, computerChoice) => {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    
+    if (humanChoice === 'rock' && computerChoice === 'rock') {
+        console.log('Draw')
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        console.log('You Lose! Paper covers Rock')
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log('You win! Rock crushes Scissors')
+    } else if (humanChoice === 'paper' && computerChoice === 'paper') {
+        console.log('Draw')
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        console.log('You Lose! Scissors cut Paper')
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log('You win! Paper covers Rock')
+    } else if (humanChoice === 'scissors' && computerChoice === 'scissors') {
+        console.log('Draw')
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        console.log('You Lose! Rock crushes Scissors')
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log('You win! Scissor cut Paper')
     }
 }
+
 playRound()
-console.log(`Human score:${humanScore} \nComputer score:${computerScore}`)
