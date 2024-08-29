cfdf887801538c7d76ae5849ea0298cb3c99173d
malopro <malovagilbert@gmail.com>
2024-08-29 17:56:48 +0200

    Added rounds

--- Rock paper scissors/script.js
+++ Rock paper scissors/script.js
@@ -3,7 +3,7 @@ const buttons = document.querySelectorAll('.btn');
 let humanScore = document.getElementById('hScore');
 let computerScore = document.getElementById('cScore');
 let board = document.querySelector('.board');
-
+let rounds = 0;
 
 let getComputerChoice = () => {
     return choice[Math.floor(Math.random() * 3)];
@@ -14,39 +14,42 @@ buttons.forEach((button) => {
         const humanChoice = button.id;
         const computerChoice = getComputerChoice();
 
-
-        if (humanChoice === computerChoice) {
-            board.textContent = 'Draw!!'
-        } else if (humanChoice === 'rock' && computerChoice === 'paper') {
-            board.textContent = 'You Lose!!'
-            let score = Number(computerScore.textContent);
-            score++
-            computerScore.textContent = score;
-        } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-            board.textContent = 'You Win!!'
-            let score = Number(humanScore.textContent);
-            score++
-            humanScore.textContent = score;
-        } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-            board.textContent = 'You Lose!!'
-            let score = Number(computerScore.textContent);
-            score++
-            computerScore.textContent = score;
-        } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-            board.textContent = 'You Win!!'
-            let score = Number(humanScore.textContent);
-            score++
-            humanScore.textContent = score;
-        } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-            board.textContent = 'You Lose!!'
-            let score = Number(computerScore.textContent);
-            score++
-            computerScore.textContent = score;
-        } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-            board.textContent = 'You Win!!'
-            let score = Number(humanScore.textContent);
-            score++
-            humanScore.textContent = score;
+        if (rounds < 5) {
+            if (humanChoice === computerChoice) {
+                board.textContent = 'Draw!!'
+            } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+                board.textContent = 'You Lose!!'
+                let score = Number(computerScore.textContent);
+                score++
+                computerScore.textContent = score;
+            } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+                board.textContent = 'You Win!!'
+                let score = Number(humanScore.textContent);
+                score++
+                humanScore.textContent = score;
+            } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+                board.textContent = 'You Lose!!'
+                let score = Number(computerScore.textContent);
+                score++
+                computerScore.textContent = score;
+            } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+                board.textContent = 'You Win!!'
+                let score = Number(humanScore.textContent);
+                score++
+                humanScore.textContent = score;
+            } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+                board.textContent = 'You Lose!!'
+                let score = Number(computerScore.textContent);
+                score++
+                computerScore.textContent = score;
+            } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+                board.textContent = 'You Win!!'
+                let score = Number(humanScore.textContent);
+                score++
+                humanScore.textContent = score;
+            }
+            rounds++;
         }
+
     })
 })
