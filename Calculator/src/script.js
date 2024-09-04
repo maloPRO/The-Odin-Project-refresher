8a580558ed883c520042af27149a67a300d7e280
maloPRO <malovagilbert@gmail.com>
2024-09-04 12:08:39 +0200

    Added add functions

--- Calculator/src/script.js
+++ Calculator/src/script.js
@@ -1,18 +1,19 @@
 const numKeys = document.querySelectorAll('.numKey');
 const funcKeys = document.querySelectorAll('.funcKey');
 const opKeys = document.querySelectorAll('.opKey');
-const equal = document.querySelector('.equal');
+const equal = document.querySelector('#equal');
 const del = document.querySelector('#del');
 const clear = document.querySelector('#clear');
 const input = document.querySelector('.input');
 const output = document.querySelector('.output');
 const keys = document.querySelectorAll('button');
+const ops = ['+', '-', '/', '*']
 
 add = (a,b) => {
-    return a + b;
+    return (a + b);
 }
 subtract = (a,b) => {
-    return a - b;
+    return (a - b);
 }
 multiply = (a,b) => {
     return a * b;
@@ -39,6 +40,17 @@ opKeys.forEach((opKey) => {
 
 clear.addEventListener('click', () => {
     input.textContent = '';
+    output.textContent = '0';
 });
 
+equal.addEventListener('click', () => {
+    const query = input.textContent;
 
+    for(let op of ops) {
+        if (op === '+') {
+            let num1 = Number(query.slice(0, query.indexOf('+')));
+            let num2 = Number(query.slice(query.indexOf('+') + 1));
+            output.textContent = add(num1, num2)
+        }
+    }
+})
