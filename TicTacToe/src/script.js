861ae54ce964fca197f5e1bb61edd50e31d301ed
maloPRO <malovagilbert@gmail.com>
2024-09-13 12:03:28 +0200

    TTC change board values to player choices

--- TicTacToe/src/script.js
+++ TicTacToe/src/script.js
@@ -1,8 +1,9 @@
 const game = (function () {
-    const board = {
-        gameBoard: ['a1', 'a2', 'a3', 'b1', 'b2','b3', 'c1', 'c2', 'c3'],
-    
-    }
+    let gameBoard = [
+            'a1', 'a2', 'a3',
+            'b1', 'b2','b3',
+            'c1', 'c2', 'c3'
+        ]
 
     const createPlayer = (name, symbol) => {
         return {name, symbol}
@@ -12,33 +13,25 @@ const game = (function () {
     const player1 = createPlayer('Player One', 'X');
     const player2 = createPlayer('Player Two', 'O');
     let currentPlayer = 1;
-    let plays = 0;
 
     const playRound = (e) => {
         
         const grid = e.target;
         if (grid.classList.contains('unclicked')) {
-            
-            grid.classList.remove('unclicked')
-            if (currentPlayer === 1 ) {
-                grid.textContent = player1.symbol;
-            } else {
-                grid.textContent = player2.symbol;
-            }
+
+            grid.classList.remove('unclicked');
+            grid.textContent = (currentPlayer === 1) ? player1.symbol : player2.symbol;
+
             currentPlayer = currentPlayer === 1 ? 2 : 1;
+            gameBoard[grid.id] = grid.textContent;
         }
-
-
-
-        
     }
-
 
     grids.forEach((grid) => {
         grid.addEventListener('click', playRound);
-
-    })
-    return {board, createPlayer}
+    });
+    
+    return {createPlayer}
 
 })();
 
