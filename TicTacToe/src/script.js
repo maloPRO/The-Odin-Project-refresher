e642ac50ab4ca824eb55b037ab7b5c93ddbf76fc
maloPRO <malovagilbert@gmail.com>
2024-09-13 08:48:40 +0200

    Added game function and players

--- TicTacToe/src/script.js
+++ TicTacToe/src/script.js
@@ -1,4 +1,34 @@
-const Gameboard = {
-    gameBoard: ['a1', 'a2', 'a3', 'b1', 'b2','b3', 'c1', 'c2', 'c3'],
+const game = (function () {
+    const board = {
+        gameBoard: ['a1', 'a2', 'a3', 'b1', 'b2','b3', 'c1', 'c2', 'c3'],
+    
+    }
 
-}
+    const createPlayer = (name, symbol) => {
+        return {name, symbol}
+    }
+
+    const grids = document.querySelectorAll('.box');
+    const player1 = createPlayer('Player One', 'X');
+    const player2 = createPlayer('Player Two', 'O');
+    let currentPlayer = 1;
+    let plays = 0;
+
+    const handleClick = (e) => {
+        plays++;
+        const grid = e.target;
+
+        if (plays < 10) {
+            currentPlayer = currentPlayer === 1 ? 2 : 1;
+            console.log(currentPlayer)
+        }
+    }
+
+    grids.forEach((grid) => {
+        grid.addEventListener('click', handleClick)
+    })
+    return {board, createPlayer}
+
+})();
+
+
