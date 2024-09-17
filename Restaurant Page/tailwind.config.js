695819e428d442fd64febce5cbb19dd6d97148f4
maloPRO <malovagilbert@gmail.com>
2024-09-17 12:21:21 +0200

    RP set up webpack with tailwind

---
+++ Restaurant Page/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.js'],
+  darkMode: false, // or 'media' or 'class',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
