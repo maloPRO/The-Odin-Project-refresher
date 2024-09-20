63d10c1ae46f0949b80406afc37f7ac8e4c8197a
malopro <malovagilbert@gmail.com>
2024-09-21 00:56:58 +0200

    Edited hero section

--- Restaurant Page/src/scripts/home.js
+++ Restaurant Page/src/scripts/home.js
@@ -4,21 +4,6 @@ import '../styles/home.css'
 export const homeBody = document.createElement('div');
 homeBody.classList.add('homeBody');
 
-const homeLeft = document.createElement('div');
-homeLeft.classList.add('homeLeft');
-homeBody.appendChild(homeLeft)
-
-const homeRight = document.createElement('div');
-homeRight.classList.add('homeRight');
-homeBody.appendChild(homeRight)
-
-const leftTop = document.createElement('div');
-leftTop.classList.add('leftTop');
-homeLeft.appendChild(leftTop);
-
-const leftBtm = document.createElement('div');
-leftBtm.classList.add('leftBtm');
-homeLeft.appendChild(leftBtm);
 
 
 
