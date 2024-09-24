52410388ebf87da7322c3eb2d667623dc9f19a34
maloPRO <malovagilbert@gmail.com>
2024-09-24 11:21:30 +0200

    RP added text shadow plugin

--- Restaurant Page/tailwind.config.js
+++ Restaurant Page/tailwind.config.js
@@ -7,11 +7,19 @@ module.exports = {
         roboto: ['Roboto', 'sans-serif'],
         dancingScript: ["Dancing Script", 'cursive'],
         archivo: ["Archivo Black", 'sans-serif']
-      }
+      },
+      textShadow: {
+        'sm': '1px 1px 2px rgba(0, 0, 0, 0.5)',
+        'md': '2px 2px 4px rgba(0, 0, 0, 0.5)',
+        'lg': '3px 3px 6px rgba(0, 0, 0, 0.5)',
+        'xl': '4px 4px 8px rgba(0, 0, 0, 0.5)',
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-textshadow'),
+  ],
 };
