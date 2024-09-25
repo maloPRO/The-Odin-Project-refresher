11bad996f453c69305e198e5cbad58eda081955d
malopro <malovagilbert@gmail.com>
2024-09-25 22:49:11 +0200

    RP added contact information

--- Restaurant Page/src/scripts/contact.js
+++ Restaurant Page/src/scripts/contact.js
@@ -18,4 +18,18 @@ export const contacts = (function() {
             main.appendChild(contactPage)
         })
     })
+
+    contactPage.innerHTML = `
+        <div class='contact-head'>CONTACT</div>
+        <div class='content'>
+            <div class='msg'>Send us a message and we will get back to you as soon as possible. Looking forward to hearing from you.</div>
+            <form>
+                <label>Name*<input type='text' required/></label>
+                <label>Email*<input type='email' required/></label>
+                <label>Phone Number*<input type='number' required/></label>
+                <label>Your Message*<textarea type='text' required></textarea></label>
+                <button type='submit'>SEND</button>
+            </form>
+        </div>
+    `
 })();
