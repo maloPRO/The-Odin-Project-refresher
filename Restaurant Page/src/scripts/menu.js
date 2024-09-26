e71a161b472ccdb9493c34fb71163375b52216fa
maloPRO <malovagilbert@gmail.com>
2024-09-26 13:02:51 +0200

    RP added food menu items

--- Restaurant Page/src/scripts/menu.js
+++ Restaurant Page/src/scripts/menu.js
@@ -7,13 +7,13 @@ import myMenu from '../assets/menu.json'
 
 export const menu = (function () {
     const menuPage = document.createElement('div');
+    menuPage.classList.add('menu-page');
+
     const menuTabs = document.querySelectorAll('.menu');
     const main = document.querySelector('.main');
-    menuPage.classList.add('menu-page');
     const images = [image1, image2, image3, image4];
     const foods = myMenu.menu.food;
     const drinks = myMenu.menu.beverages;
-
     const mobileMenu = document.querySelector('.mobile-menu');
 
     menuTabs.forEach((tab) => {
@@ -40,27 +40,45 @@ export const menu = (function () {
     
     menuContent.innerHTML = `
         <div class='menuType'>
-            <button>FOOD MENU</button>
-            <button>DRINK MENU</button>
+            <button class='foodBtn'>FOOD MENU</button>
+            <button class='drinkBtn'>DRINK MENU</button>
         </div>
     `
 
     const foodMenu = document.createElement('div');
     foodMenu.classList.add('foodMenu');
-    menuContent.appendChild(foodMenu)
     foodMenu.innerHTML = `
         <div class='menuTitle'>FOODS</div>
     `
+
+    const drinkMenu = document.createElement('div');
+    drinkMenu.classList.add('drinkMenu');
+    drinkMenu.innerHTML = `
+        <div class='menuTitle'>DRINKS</div>
+    `
+    
+    menuContent.appendChild(foodMenu)
 
     foods.forEach((food) => {
         const foodCard = document.createElement('div');
         foodCard.classList.add('foodCard');
         foodCard.innerHTML = `
-            <div class='foodName'>${food.name}</div>
-            <div class='foodDesc'>${food.description}</div>
-            <div class='foodPrice'>$ ${food.price}</div>
+            <div class='itemName'>${food.name}</div>
+            <div class='itemDesc'>${food.description}</div>
+            <div class='itemPrice'>$ ${food.price}</div>
         `
         foodMenu.appendChild(foodCard)
+    });
+
+    drinks.forEach((drink) => {
+        const drinkCard = document.createElement('div');
+        drinkCard.classList.add('drinkCard');
+        drinkCard.innerHTML = `
+            <div class='itemName'>${drink.name}</div>
+            <div class='itemDesc'>${drink.description}</div>
+            <div class='itemPrice'>$ ${drink.price}</div>
+        `
+        drinkMenu.appendChild(drinkCard)
     })
 
     let idx = 0;
